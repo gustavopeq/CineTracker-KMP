c5d276d7749ccfe067a40ddcc801fdabf4d3d40a
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2024-06-22 23:43:57 -0700

    Updated Android default themes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.appcompat)
 
             implementation(libs.ktor.client.okhttp)
         }
--- composeApp/src/androidMain/kotlin/com/projects/cinetracker/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/projects/cinetracker/MainActivity.kt
@@ -4,24 +4,13 @@ import MainAppView
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.projects.cinetracker.system.SetStatusBarColor
-import common.ui.theme.PrimaryBlackColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SetStatusBarColor(PrimaryBlackColor)
             MainAppView()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    MainAppView()
 }
