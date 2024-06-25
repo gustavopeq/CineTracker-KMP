2de41b26a83da3a491cb7bbf23e5c18b5166c717
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2024-06-24 17:38:01 -0700

    Updated iOS safeArea

---
+++ iosApp/iosApp/Util/Extensions.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Color {
+    init(hex: UInt) {
+        self.init(
+            red: Double((hex >> 16) & 0xFF) / 255.0,
+            green: Double((hex >> 8) & 0xFF) / 255.0,
+            blue: Double(hex & 0xFF) / 255.0
+        )
+    }
+}
