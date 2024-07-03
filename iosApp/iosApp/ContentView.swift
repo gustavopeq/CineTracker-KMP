197881bc0a343e807713a4ab43b4326c0fed99f3
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2024-07-02 17:58:01 -0700

    Fixed screen offset to accomodate system status bar

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -11,15 +11,28 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
+    var statusBarHeight = {
+        return UIApplication.shared.windows.first?.safeAreaInsets.top
+    }
+    var systemNavBarHeight = {
+        return UIApplication.shared.windows.first?.safeAreaInsets.bottom
+    }
+    
     var body: some View {
-        ZStack {
-            Color(hex: 0x191A1D)
-                .ignoresSafeArea()
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Color.black
+                    .frame(height: geometry.size.height / 2)
+                Color(hex: 0x191A1D)
+            }
+            .ignoresSafeArea()
+
             ComposeView()
-                .padding(.bottom, 32)
+                .padding(.bottom, systemNavBarHeight())
+                .padding(.top, statusBarHeight())
                 .ignoresSafeArea(edges: .all)
                 .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
-            }
         }
+    }
 }
 
