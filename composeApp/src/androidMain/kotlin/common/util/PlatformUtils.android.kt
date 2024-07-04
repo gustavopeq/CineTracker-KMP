07dcccabddf0c631d00ed1db7ce6380316e65f42
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2024-07-03 19:27:05 -0700

    Added localization PT, and ES

--- composeApp/src/androidMain/kotlin/common/util/PlatformUtils.android.kt
+++ composeApp/src/androidMain/kotlin/common/util/PlatformUtils.android.kt
@@ -1,5 +1,15 @@
 package common.util
 
+import java.util.Locale
+
 actual object PlatformUtils {
     actual val isIOS: Boolean = false
+    actual fun getUserLanguage(): String = Locale.getDefault().language
+    actual fun getUserCountry(): String = Locale.getDefault().country
+    actual fun getLocale(): String {
+        val language = getUserLanguage()
+        val country = getUserCountry()
+
+        return "$language-$country"
+    }
 }
--- composeApp/src/iosMain/kotlin/common/util/PlatformUtils.ios.kt
+++ composeApp/src/iosMain/kotlin/common/util/PlatformUtils.ios.kt
@@ -1,5 +1,24 @@
 package common.util
 
+import platform.Foundation.NSLocale
+import platform.Foundation.countryCode
+import platform.Foundation.currentLocale
+import platform.Foundation.preferredLanguages
+
 actual object PlatformUtils {
     actual val isIOS: Boolean = true
+    actual fun getUserLanguage(): String {
+        val languageTag = (NSLocale.preferredLanguages.firstOrNull() as String?)
+        return languageTag?.split("-")?.first() ?: DEFAULT_LANGUAGE
+    }
+    actual fun getUserCountry(): String = NSLocale.currentLocale.countryCode ?: DEFAULT_COUNTRY
+    actual fun getLocale(): String {
+        val language = getUserLanguage()
+        val country = getUserCountry()
+
+        return "$language-$country"
+    }
 }
+
+const val DEFAULT_LANGUAGE = "en"
+const val DEFAULT_COUNTRY = "US"
