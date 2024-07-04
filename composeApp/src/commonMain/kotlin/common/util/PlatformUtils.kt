ba9f20233cc4284347ad31c08295a951b034021c
gustavopequeno <gustavo.pequeno@crunchyroll.com>
2024-07-03 19:27:05 -0700

    Added localization PT, and ES

--- composeApp/src/commonMain/kotlin/common/util/PlatformUtils.kt
+++ composeApp/src/commonMain/kotlin/common/util/PlatformUtils.kt
@@ -2,4 +2,7 @@ package common.util
 
 expect object PlatformUtils {
     val isIOS: Boolean
+    fun getUserLanguage(): String
+    fun getUserCountry(): String
+    fun getLocale(): String
 }
--- composeApp/src/commonMain/kotlin/core/LanguageManager.kt
+++ composeApp/src/commonMain/kotlin/core/LanguageManager.kt
@@ -1,19 +1,45 @@
 package core
 
-import common.domain.util.Constants.DEFAULT_LANGUAGE_TAG
+import common.util.PlatformUtils.getLocale
+import common.util.PlatformUtils.getUserCountry
+import common.util.PlatformUtils.getUserLanguage
 
 object LanguageManager {
+    private val supportedLanguages = listOf(
+        "en-US",
+        "en-CA",
+        "pt-BR",
+        "es-ES",
+        "es-MX",
+        "es-BO",
+        "es-CL",
+        "es-CO",
+        "es-EC",
+        "es-PY",
+        "es-PE",
+        "es-PR",
+        "es-UY",
+        "es-VE",
+        "es-CR",
+    )
+    private const val DEFAULT_LANGUAGE = "en-US"
+
     fun getUserLanguageTag(): String {
-//        val languageTag = userLocale.value.toLanguageTag()
-//        val language = userLocale.value.language
-//        return when {
-//            languageTag.isSupported() -> languageTag
-//            language == "pt" -> "pt-BR"
-//            language == "es" -> "es-ES"
-//            else -> DEFAULT_LANGUAGE
-//        }
-        return DEFAULT_LANGUAGE_TAG
+        val languageTag = getLocale()
+        val language = getUserLanguage()
+        println("languageTag: $languageTag - $language")
+
+        return when {
+            languageTag.isSupported() -> languageTag
+            language == "pt" -> "pt-BR"
+            language == "es" -> "es-ES"
+            else -> DEFAULT_LANGUAGE
+        }
     }
 
-    fun getUserCountryCode(): String = "US" // userLocale.value.country
+    private fun String.isSupported(): Boolean {
+        return supportedLanguages.contains(this)
+    }
+
+    fun getUserCountryCode(): String = getUserCountry()
 }
