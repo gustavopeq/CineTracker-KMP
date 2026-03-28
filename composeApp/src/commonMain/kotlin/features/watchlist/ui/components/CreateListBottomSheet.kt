36bc72107684dd8a287af4e2929c37c114722514
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2026-03-28 19:49:41 -0300

    Fix iOS system bar colors with edge-to-edge and clean up StatusBarUpdate

--- composeApp/src/commonMain/kotlin/features/watchlist/ui/components/CreateListBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/features/watchlist/ui/components/CreateListBottomSheet.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cinetracker_kmp.composeapp.generated.resources.Res
