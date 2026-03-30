ca3a04160189142a3979c2c667222073598bd636
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2026-03-30 19:31:39 -0300

    Initialize Sentry on Android and iOS with environment detection

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,13 +1,28 @@
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.projects.cinetracker.BuildKonfig
 import common.util.platform.AppHaptics
+import common.util.platform.PlatformUtils
 import core.di.KoinInitializer
+import io.sentry.kotlin.multiplatform.Sentry
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
         KoinInitializer().init()
+        initSentry()
         AppHaptics.warmUp()
     }
 ) {
     MainAppView()
+}
+
+private fun initSentry() {
+    val dsn = BuildKonfig.SENTRY_DSN
+    if (dsn.isNotEmpty()) {
+        Sentry.init { options ->
+            options.dsn = dsn
+            options.environment = if (PlatformUtils.isDebugBuild) "debug" else "release"
+            options.debug = PlatformUtils.isDebugBuild
+        }
+    }
 }
