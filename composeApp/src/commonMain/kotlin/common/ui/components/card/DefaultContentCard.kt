7e2e1e536e7e28cdfd33b8bc74778f292d1f6863
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2026-04-01 22:16:47 -0300

    Remove unused SharedTransitionScope import from DefaultContentCard

--- composeApp/src/commonMain/kotlin/common/ui/components/card/DefaultContentCard.kt
+++ composeApp/src/commonMain/kotlin/common/ui/components/card/DefaultContentCard.kt
@@ -1,6 +1,5 @@
 package common.ui.components.card
 
-import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
