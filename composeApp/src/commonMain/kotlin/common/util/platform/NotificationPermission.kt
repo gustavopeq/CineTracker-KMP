5aef2ed43c620029e4483b9ef500ad3f1dba03c1
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2026-04-03 11:57:01 -0300

    Add notification permission expect/actual composable

---
+++ composeApp/src/commonMain/kotlin/common/util/platform/NotificationPermission.kt
@@ -0,0 +1,8 @@
+package common.util.platform
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun rememberNotificationPermissionLauncher(
+    onResult: (Boolean) -> Unit,
+): () -> Unit
