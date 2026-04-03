48bdf941835752055d220e5a088becaef097b2ed
Gustavo Pequeno <gustavo_pequeno2@hotmail.com>
2026-04-03 11:58:39 -0300

    Wire notification initialization and reschedule on app startup

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -2,18 +2,29 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import com.projects.cinetracker.BuildKonfig
 import common.util.platform.AppHaptics
+import common.util.platform.AppNotifications
 import common.util.platform.PlatformUtils
 import core.di.KoinInitializer
+import database.repository.SettingsRepository
 import io.sentry.kotlin.multiplatform.Sentry
+import org.koin.mp.KoinPlatform
 
 fun mainViewController() = ComposeUIViewController(
     configure = {
         KoinInitializer().init()
         initSentry()
         AppHaptics.warmUp()
+        rescheduleEngagementRemindersIfEnabled()
     }
 ) {
     MainAppView()
+}
+
+private fun rescheduleEngagementRemindersIfEnabled() {
+    val settingsRepository: SettingsRepository = KoinPlatform.getKoin().get()
+    if (settingsRepository.areEngagementRemindersEnabled()) {
+        AppNotifications.scheduleEngagementReminders()
+    }
 }
 
 private fun initSentry() {
